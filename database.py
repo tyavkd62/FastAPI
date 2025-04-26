e6e552a5958fc00bdf574b5edc5c86e58240bed1
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-26 20:35:44 +0900

    MySQL로 전환

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+'''
+PyMySQL로 전환'''
+import pymysql
+pymysql.install_as_MySQLdb()
 
 SQLALCHEMY_DATABASE_URL = 'mysql+mysqldb://root:test@127.0.0.1/fastapi_ca'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
