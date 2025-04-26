efbc99ac1939dfca50f7773d5c2cb4166b860b7a
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-26 16:55:52 +0900

    API 라우터 추가

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
+'''
+FastAPI에서 라우터를 사용하도록 설정합니다'''
+from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
+app.include_router(user_routers)
 
 @app.get('/')
 def hello():
