dcc02cd405513fec3948e0fddb7a507378b61799
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-26 16:38:59 +0900

    중복 유저 검사 기능 추가

--- user/domain/repository/user_repo.py
+++ user/domain/repository/user_repo.py
@@ -9,3 +9,10 @@ class IUserRepository(metaclass=ABCMeta):
     @abstractmethod
     def save(self, user: User):
         raise NotImplementedError
+    
+    @abstractmethod
+    def find_by_email(self, email: str) -> User:
+        '''
+        이메일로 유저를 검색하고,
+        검색한 유저가 없을 경우 422 에러를 발생시킵니다.'''
+        raise NotImplementedError
