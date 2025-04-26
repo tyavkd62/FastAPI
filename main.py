f581736fdf3523585c3b8b25b372ae56c9d1e7ea
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-26 17:23:51 +0900

    400 Bad Request로 변경하기

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ import uvicorn
 '''
 FastAPI에서 라우터를 사용하도록 설정합니다'''
 from user.interface.controllers.user_controller import router as user_routers
+'''
+유효성 검사 오류의 상태 코드를 400 Bad Request로 변경하기'''
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 app.include_router(user_routers)
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+    )
+    
 @app.get('/')
 def hello():
     return {'Hello': 'FastAPI'}
