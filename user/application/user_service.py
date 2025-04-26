f0f1e2e70699d9186c3420563c070b74d44e307a
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-26 16:45:26 +0900

    패스워드 암호화 기능 구현

--- user/application/user_service.py
+++ user/application/user_service.py
@@ -9,6 +9,9 @@ from user.infra.repository.user_repo import UserRepository
 '''
 유저를 데이터베이스에 저장하기 전에 이미 가입되어 있는 유저인지 검사하는 기능을 구현합니다'''
 from fastapi import HTTPException
+'''
+유저를 생성할 때 패스워드를 암호화해서 저정하는 기능을 구현합니다'''
+from utils.crypto import Crypto
 
 class UserService:
     def __init__(self):
@@ -32,7 +35,7 @@ class UserService:
             id=self.ulid.generate(),
             name=name,
             email=email,
-            password=password,
+            password=self.crypto.encrypt(password),
             created_at=now,
             updated_at=now,
         )
