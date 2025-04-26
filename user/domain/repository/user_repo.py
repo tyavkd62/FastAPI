e691463f67128e3c721733ca6e703d228369f0b9
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-26 21:07:20 +0900

    abstractmethod 임포트

--- user/domain/repository/user_repo.py
+++ user/domain/repository/user_repo.py
@@ -2,7 +2,7 @@
 IUserRepository 클래스는 파이썬에서 제공하는 객체지향 인터페이스로 선언하기 위해
 ABCMeta 클래스를 이용한다'''
 
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from user.domain.user import User
 
 class IUserRepository(metaclass=ABCMeta):
--- user/infra/repository/user_repo.py
+++ user/infra/repository/user_repo.py
@@ -1,4 +1,26 @@
+from database import SessionLocal
 from user.domain.repository.user_repo import IUserRepository
+from user.domain.user import User as UserVO
+from user.infra.db_models.user import User
 
 class UserRepository(IUserRepository):
-    pass
+    def save(self, user: UserVO):
+        new_user = User(
+            id=user.id,
+            email=user.email,
+            name=user.name,
+            password=user.password,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
+        )
+        
+        with SessionLocal() as db:
+            try:
+                db = SessionLocal()
+                db.add(new_user)
+                db.commit()
+            finally:
+                db.close()
+            
+    def find_by_email(self, email: str) -> User:
+        pass
