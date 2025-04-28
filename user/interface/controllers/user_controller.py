c9445fadf736827c212fc4286901bbd31e12df09
tyavkd62 <107627503+tyavkd62@users.noreply.github.com>
2025-04-28 22:09:37 +0900

    파이단틱 유효성 검사 기능 추가

--- user/interface/controllers/user_controller.py
+++ user/interface/controllers/user_controller.py
@@ -7,7 +7,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 '''
 회원 가입 라우터로 전달된 외부의 요청에 포함돼 있는 본문을 검사하는 기능을 구현합니다'''
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 '''
 유저 생성 유스 케이스 호출'''
 from user.application.user_service import UserService
@@ -15,9 +15,9 @@ from user.application.user_service import UserService
 router = APIRouter(prefix='/users') # FastAPI가 제공하는 APIRouter 객체를 생성합니다
 
 class CreateUserBody(BaseModel): # 파이단틱의 BaseModel을 상속받아 파이단틱 모델을 선언합니다
-    name: str
-    email: str
-    password: str
+    name: str = Field(min_length=2, max_length=32)
+    email: EmailStr = Field(max_length=64)
+    password: str = Field(min_length=8, max_length=32)
 
 @router.post('', status_code=201, response_model=None) # post 메서드를 이용해 /users 라는 경로로 POST 요청을 받을 수 있습니다
 @inject
@@ -34,8 +34,8 @@ def create_user(
     return created_user
 
 class UpdateUser(BaseModel):
-    name: str | None = None
-    password: str | None = None
+    name: str | None = Field(min_length=2, max_length=32, default=None)
+    password: str | None = Field(min_length=8, max_length=32, default=None)
     
 @router.put("/{user_id}")
 @inject
